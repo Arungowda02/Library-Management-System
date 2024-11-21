5ed05ef57ad25b8628e7e078a39dbec64187feaf
Arun Gowda <arungowdaarungowda36@gmail.com>
2024-11-21 16:24:58 +0530

    Adding interfaces of book into git from interfaceBranch

---
+++ library_management_system/interfaces/book/AddAndRemoveBook.java
@@ -0,0 +1,8 @@
+package interfaces.book;
+
+import entities.Book;
+
+public interface AddAndRemoveBook {
+    void addBook(Book book);
+    void removeBook(Book book);
+}
---
+++ library_management_system/interfaces/book/Book.java
@@ -0,0 +1,7 @@
+package interfaces.book;
+
+public interface Book {
+    void updateAvailability(boolean status);
+    String getDetails();
+
+}
